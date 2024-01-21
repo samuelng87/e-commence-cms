a3a2b289330576b1087bc392575f165d04d347d2
sam <samuel6249ng@gmail.com>
2024-01-21 12:46:23 +1100

    radix and clerk packages for user button

---
+++ components/ui/modal.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+interface ModalProps {
+    title: string;
+    description: string;
+    isOpen: boolean;
+    onClose: () => void;
+    children?: React.ReactNode;
+};
+
+export const Modal: React.FC<ModalProps> = ({
+    title,
+    description,
+    isOpen,
+    onClose,
+    children
+}) => {
+    const onChange = (open: boolean) => {
+        if (!open) {
+            onClose();
+        }
+    }
+}
+
+export default Modal
