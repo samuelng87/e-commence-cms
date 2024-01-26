219974c564f3cd020f37941f84b36679d0c6bbda
sam <samuel6249ng@gmail.com>
2024-01-26 15:19:57 +1100

    working on Category logics and rendering billboards

--- app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -5,42 +5,40 @@ import Heading from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
 import { Plus } from "lucide-react"
 import { useParams, useRouter } from "next/navigation"
-import { BillboardColumn, columns } from "./columns"
+import { CategoryColumn, columns } from "./columns"
 import { DataTable } from "@/components/ui/data-table"
 import { ApiList } from "@/components/ui/api-list"
 
 // npx shadcn-ui@latest add table, npm install @tanstack/react-table
 
-
-
-
-interface BillboardClientProps {
-  data: BillboardColumn[];
+interface CategoryClientProps {
+  data: CategoryColumn[];
 }
 
-export const BillboardClient: React.FC<BillboardClientProps> = ({
+export const CategoryClient: React.FC<CategoryClientProps> = ({
   data
 }) => {
   const params = useParams();
   const router = useRouter();
 
-  console.log(data.length, 'bill board client')
+  console.log(data.length, 'categories client')
   return (
     <>
       <div className="flex items-center justify-between">
         <Heading 
-          title={`Billboards (${data.length})`} 
-          description="Manage billboards for your store" 
+          title={`Categories (${data.length})`} 
+          description="Manage categories for your store" 
           />
-        <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
-          <Plus className="mr-2 h-4 w-4" /> Add New
+        <Button onClick={() => router.push(`/${params.storeId}/categories/new`)}>
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
         </Button>
       </div>
       <Separator />
       <DataTable columns={columns} data={data} searchKey="label"/>
-      <Heading title="API" description="Easy access for API Billboards" />
+      <Heading title="API" description="Easy access for API Categories" />
       <Separator />
-      <ApiList entityName="billboards" entityIdName="billboardId"/>
+      <ApiList entityName="categories" entityIdName="categoryId"/>
     </>
   );
 };
---
+++ app/(dashboard)/[storeId]/(routes)/categories/components/columns.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import { CellAction } from "./cell-action"
+import Image from "next/image";
+
+
+export type CategoryColumn = {
+  id: string
+  name: string
+  billboardLabel: string
+  imageUrl: string
+  createdAt: string
+}
+
+export const columns: ColumnDef<CategoryColumn>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    accessorKey: "billboard",
+    header: "Billboard",
+    cell : ({ row }) => row.original.billboardLabel,
+  },
+  // {
+  //   accessorKey: "imageUrl",
+  //   header: "Image",
+  //   cell : ({ row }) => {
+  //     const original = row.original as CategoryColumn;
+  //     return (
+  //       <Image
+  //         className="rounded-md"
+  //         height="100"
+  //         width="100"
+  //         alt="billboards listing image"
+  //         src={original.imageUrl} 
+  //        />
+  //       )
+  //   },
+  // },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ( {row } ) => <CellAction data={row.original}/>
+  },
+];
