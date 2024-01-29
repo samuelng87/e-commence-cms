1fa749523c58e253ffed935250489cb1cc576fa1
sam <samuel6249ng@gmail.com>
2024-01-29 15:30:41 +1100

    product page CRUD and merging tables: color,size,catetogry

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -0,0 +1,47 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import Heading from "@/components/ui/heading"
+import { Separator } from "@/components/ui/separator"
+import { Plus } from "lucide-react"
+import { useParams, useRouter } from "next/navigation"
+import { ProductColumn, columns } from "./columns"
+import { DataTable } from "@/components/ui/data-table"
+import { ApiList } from "@/components/ui/api-list"
+
+// npx shadcn-ui@latest add table, npm install @tanstack/react-table
+
+
+
+
+interface ProductClientProps {
+  data: ProductColumn[];
+}
+
+export const ProductClient: React.FC<ProductClientProps> = ({
+  data
+}) => {
+  const params = useParams();
+  const router = useRouter();
+
+  console.log(data.length, 'Product client')
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading 
+          title={`Products (${data.length})`} 
+          description="Manage product for your store" 
+          />
+        <Button onClick={() => router.push(`/${params.storeId}/products/new`)}>
+          <Plus className="mr-2 h-4 w-4" /> Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="name"/>
+      <Heading title="API" description="Easy access for API products" />
+      <Separator />
+      <ApiList entityName="products" entityIdName="productId"/>
+    </>
+  );
+};
+
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -38,11 +38,11 @@ export function MainNav({
       label: 'Colors',
       active: pathname === `/${params.storeId}/colors`,
     },
-    // {
-    //   href: `/${params.storeId}/products`,
-    //   label: 'Products',
-    //   active: pathname === `/${params.storeId}/products`,
-    // },
+    {
+      href: `/${params.storeId}/products`,
+      label: 'Products',
+      active: pathname === `/${params.storeId}/products`,
+    },
     // {
     //   href: `/${params.storeId}/orders`,
     //   label: 'Orders',
